299dc7aaab3b07d00019de72a294d247d5cda8c3
Yosgi <siqi.liu@nextspace.com>
2025-01-10 22:07:21 +1300

    Update title and description in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,9 +3,9 @@ import { TopNavBar } from "components/TopNavBar";
 import { Analytics } from "@vercel/analytics/react";
 
 export const metadata = {
-  title: "OpenResume - Free Open-source Resume Builder and Parser",
+  title: "NiceResume - Nice Resume Builder",
   description:
-    "OpenResume is a free, open-source, and powerful resume builder that allows anyone to create a modern professional resume in 3 simple steps. For those who have an existing resume, OpenResume also provides a resume parser to help test and confirm its ATS readability.",
+    "free, open-source, and powerful resume builder,based on Open Resume",
 };
 
 export default function RootLayout({
